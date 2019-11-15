71d212862cf830d7344dfc3a7aec0f1129ac7ce9
domotord90 <jazzmaster@freemail.com>
2019-11-15 18:56:42 +0100

    Updates

---
+++ precache-manifest.59cb6edea85dbe3e3fedfa351f109ef5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371fee626c199ec49d6b7748c6260000",
+    "url": "/github.io/react-memory-game/index.html"
+  },
+  {
+    "revision": "3fdf0216470c1c64fda6",
+    "url": "/github.io/react-memory-game/static/css/main.6122a070.chunk.css"
+  },
+  {
+    "revision": "bc1cbe2ddfade9d77b68",
+    "url": "/github.io/react-memory-game/static/js/2.8785c6f2.chunk.js"
+  },
+  {
+    "revision": "3fdf0216470c1c64fda6",
+    "url": "/github.io/react-memory-game/static/js/main.df4ad747.chunk.js"
+  },
+  {
+    "revision": "1d36c41d98338b7e517e",
+    "url": "/github.io/react-memory-game/static/js/runtime-main.083dda1b.js"
+  }
+]);
